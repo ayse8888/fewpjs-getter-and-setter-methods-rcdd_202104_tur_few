88e0a76b5ebc0c3a21b1b6ce712ffc31663f2dd6
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-05-23 21:06:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  
+   get speak() {
+    return `${this.diameter} ${this.circumference} ${this.area}`;
+  }
+  
+  set areaCircle(diameter,circumference,area) {
+    this.diameter = diameter;
+    this.circumference = circumference;
+    this.area = area;
+  }
+}
+ 
+  let circle = new Circle(Math.PI);
+  circle
